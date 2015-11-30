151459d870bdfcd1c74fcc600b871159ebb5dab4
Piotr Kedzia <piotr@kedzia.eu>
2015-11-30 21:04:24 +0100

    Use high_voltage gem for serving static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get "pages/:page" => "pages#show"
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
